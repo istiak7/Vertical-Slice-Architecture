87600613f04d1cca52df39e30dca3407d9b3ca76
istiak7 <istiak6069@gmail.com>
2026-06-29 12:48:55 +0600

    added base repository

---
+++ Vertical-Slice-Architecture/Shared/Repository/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vertical_Slice_Architecture.Database;
+using Vertical_Slice_Architecture.Entities;
+
+namespace Vertical_Slice_Architecture.Shared.Repository
+{
+    public class BaseRepository<T>(AppDbContext context) : IBaseRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context = context;
+
+        #region GET
+        public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<T>().FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        #endregion
+
+        #region POST
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddAsync(entity, cancellationToken);       
+        }
+
+        public Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _context.Set<T>().Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                
+            }
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        #endregion
+    }
+}
---
+++ Vertical-Slice-Architecture/Shared/Repository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Vertical_Slice_Architecture.Entities;
+
+namespace Vertical_Slice_Architecture.Shared.Repository
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
